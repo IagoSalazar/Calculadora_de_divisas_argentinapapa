e61580580f1ca7b5bb819e0071e8b30fe0aa665b
IagoSalazar <119471115+IagoSalazar@users.noreply.github.com>
2023-08-24 17:47:46 -0300

    se corrigio el codigo de JS

--- appIng.js
+++ appIng.js
@@ -1,6 +1,6 @@
 function calDolar(){
-    let dolar = document.getElementById("dolar");
-    let num = document.getElementById("peso2");
+    let dolar = document.querySelector('#dolar_input').value;
+    let num = document.querySelector("#peso2").value;
     let resultado = num / dolar;
     resultado = resultado.toFixed(2);
     let mensaje = `tienes un saldo de $${resultado} dolares en pesos`;
@@ -8,8 +8,8 @@ function calDolar(){
     return mensaje;
 }
 function calEuro(){
-    let euro = document.getElementById("euro");
-    let num = document.getElementById("peso1");
+    let euro = document.querySelector("#euro_input").value;
+    let num = document.querySelector("#peso1").value;
     let resultado = num / euro;
     resultado = resultado.toFixed(2);
     let mensaje = `tienes un saldo de $${resultado} euros en pesos`;
